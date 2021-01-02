7f47d4bdf512e5efe2f6173a4e92060114f95267
Ilya <viktorov.200@mail.ru>
2021-01-02 17:42:30 +0700

    Final

--- Main.java
+++ Main.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class Main{
 	public static void main(String[] args){
-		// Car car = new Car();
-		// car.Init();	
-		// car.Read();
-		// car.Display();
-		System.setProperty("console.encoding","windows-1251");
-		var toyota = new Car();
+	
+	var toyota = new Car();
     Car bmw = new Car();
     Car lamba = new Car();
     Shop diler_1 = new Shop();
     Shop diler_2 = new Shop();
 
-    toyota.Init("toyota", "carib", 83, "автомат", "зелёный", 1998, 100000);
-    bmw.Init("bmw", "m5", 200, "автомат", "чёрный", 2020, 300000);
-    lamba.Init("lamba", "veneno", 400, "автомат", "красный", 2019, 400000);
+    toyota.Init("toyota", "carib", 83, "mechanics", "green", 1998, 100000);
+    bmw.Init("bmw", "m5", 200, "automatic", "silver", 2020, 300000);
+    lamba.Init("lamba", "veneno", 400, "automatic", "red", 2019, 400000);
 
 	diler_1.Init(toyota);
 	diler_2.Init();
@@ -40,11 +37,11 @@ class Shop{
 
 	public void Init() {
         slot.Init();
-        classCar = "Не указано";
+        classCar = "None";
 	}
 	public void Init(Car slot) {
         this.slot = slot;
-        classCar = "Не указано";
+        classCar = "None";
     }
     public void Read() {
         slot.Read();
@@ -53,16 +50,24 @@ class Shop{
         slot.Display(); 
 	}
 	public void ChangeClass() {
+		System.out.print("Class auto: ");
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Укажите класс машины: ");
 		this.classCar = scan.nextLine();
 	}
 	public void InfoClass() {
-		System.out.println("Класс машины: " + classCar);
+		try{
+		PrintStream out = new PrintStream(System.out, true, "Windows-1251");
+		out.println("Class auto: " + classCar);
+		}catch(Exception err){
+			err.printStackTrace();}	
     }
     public void InfoBalance() {
-        System.out.print("Баланс: ");
-		System.out.println(balance);
+        try{
+		PrintStream out = new PrintStream(System.out, true, "Windows-1251");
+		out.print("Balance: ");
+		out.println(balance);}
+		catch(Exception err){
+			err.printStackTrace();}	
     }
     public void SaleCar() {
         balance = slot.Sale(balance);
@@ -114,13 +119,18 @@ class Car{
 		}
 	}
 	public void Display(){
-		System.out.println(marka);
-		System.out.println(model);
-		System.out.println(power);
-		System.out.println(transmission);
-		System.out.println(color);
-		System.out.println(year);
-		System.out.println(price + "\n");
+		try{
+			PrintStream out = new PrintStream(System.out, true, "Windows-1251");
+		out.println(marka);
+		out.println(model);
+		out.println(power);
+		out.println(transmission);
+		out.println(color);
+		out.println(year);
+		out.println(price + "\n");
+		}catch(Exception err){
+			err.printStackTrace();
+		}
 	}
 	public int Sale(int _balance){
 		_balance += price;
@@ -129,7 +139,11 @@ class Car{
 	public void Paint() {
 		String newColor;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("\nСтарый цвет: "+ color + "\tНовый цвет");
+		try{
+			PrintStream out = new PrintStream(System.out, true, "Windows-1251");
+		out.println("\nOld color: "+ color + "\nNew color: ");}
+		catch(Exception err){
+			err.printStackTrace();}
         newColor = scan.nextLine();
         color = newColor;
     }
